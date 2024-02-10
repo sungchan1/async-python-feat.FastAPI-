679375bbc34f0d738c4fb4778d3a7120eb97a6fe
sungchan Yeo <sungchan254@naver.com>
2024-02-10 22:34:21 +0900

    finish async lecture

--- 6-실전-프로젝트-콜렉터스/1 ~ 6/app/main.py
+++ 6-실전-프로젝트-콜렉터스/1 ~ 6/app/main.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from app.models import mongodb
 from app.models.book import BookModel
 
+
+
+
 BASE_DIR = Path(__file__).resolve().parent
 
 
@@ -25,26 +28,21 @@ async def root(request: Request):
 @app.get("/search", response_class=HTMLResponse)
 async def root(request: Request, q: str):
     print(q)
+    book = BookModel(keyword="python", publisher='BJPublic', price=1200, image='me.png')
+    await mongodb.engine.save(book)
     return templates.TemplateResponse(
         "./index.html",
         {"request": request, "title": "콜렉터 북북이", "keyword":q}
     )
-# @app.get("/search", response_class=HTMLResponse)
-# async def search(request: Request, s: str):
-#     # print(q)
-#     return templates.TemplateResponse(
-#         "./index.html",
-#         {"request": request, "title": "콜렉터 북북이"},
-#     )
+
+@app.on_event("startup")
+def on_app_start():
+    """before app starts"""
+    mongodb.connect()
+
 #
 #
-# @app.on_event("startup")
-# def on_app_start():
-#     """before app starts"""
-#     mongodb.connect()
-#
-#
-# @app.on_event("shutdown")
-# def on_app_shutdown():
-#     """after app shutdown"""
-#     mongodb.close()
+@app.on_event("shutdown")
+def on_app_shutdown():
+    """after app shutdown"""
+    # mongodb.close()
